fac64f143d38b4082b2ae39306bdac41f09ee1f5
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-04-25 11:39:38 +0530

    Add Clerk authentication integration and create sign-in/sign-up pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from '@clerk/nextjs'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      HomePage
+
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+    </div>
   )
 }
 
